7aea0d5ea25ad1199a759dacf7228f349a4e7374
P3TE <thisisntanemailaccount@gmail.com>
2021-01-13 17:31:58 +1000

    If the destination is None, ignore the incoming message.

--- src/ros_tcp_endpoint/client.py
+++ src/ros_tcp_endpoint/client.py
@@ -176,6 +176,11 @@ class ClientThread(Thread):
 
                 data += packet
 
+            if destination is None:
+                print("Received an empty destination, ignoring.")
+                self.shutdown_client()
+                return
+
             if not data and full_message_size > 0:
                 print("No data for a message size of {}, breaking!".format(full_message_size))
                 self.shutdown_client()
